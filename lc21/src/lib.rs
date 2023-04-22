904c7b4a8e562d72ddeab90fa6c7f2fce21fbc18
Matheus Cardoso <dacachmatheus@gmail.com>
2023-04-22 15:47:52 -0300

    [Easy] Leetcode 21

---
+++ lc21/src/lib.rs
@@ -0,0 +1,35 @@
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+pub fn merge_two_lists(
+    list1: Option<Box<ListNode>>,
+    list2: Option<Box<ListNode>>,
+) -> Option<Box<ListNode>> {
+    match (list1, list2) {
+        (None, None) => None,
+        (None, Some(list)) | (Some(list), None) => Some(list),
+        (Some(l1), Some(l2)) => {
+            if l1.val <= l2.val {
+                Some(Box::new(ListNode {
+                    val: l1.val,
+                    next: merge_two_lists(l1.next, Some(l2)),
+                }))
+            } else {
+                Some(Box::new(ListNode {
+                    val: l2.val,
+                    next: merge_two_lists(Some(l1), l2.next),
+                }))
+            }
+        }
+    }
+}
